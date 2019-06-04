af4de247e60ff19dbe5124d5a5ed706ebc6a8315
DEV\YVKrylova <YVKrylova>
2019-06-04 16:56:13 +0300

    cassandra tutorial

--- fancy-user-service/src/main/java/com/jstik/fancy/account/discovery/DiscoveryClientController.java
+++ fancy-user-service/src/main/java/com/jstik/fancy/account/discovery/DiscoveryClientController.java
@@ -1,12 +1,15 @@
 package com.jstik.fancy.account.discovery;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.net.URISyntaxException;
 
 @RestController()
 @RefreshScope
@@ -24,7 +27,13 @@ public class DiscoveryClientController {
 
 
     @GetMapping("/${account.activation.url}")
-    public Object choose(){
-        return activationUrl;
+    public Object choose() throws URISyntaxException {
+        ServiceInstance instance = loadBalancerClient.choose(applicationName);
+        String contextPath = instance.getMetadata().get("contextPath");
+
+        URIBuilder builder = new URIBuilder(instance.getUri());
+
+        builder.setPathSegments(contextPath, activationUrl);
+        return builder.build();
     }
 }
