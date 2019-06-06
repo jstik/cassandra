ca97e97ebf9f7a4f06ecccc0835ae67245016493
DEV\YVKrylova <YVKrylova>
2019-06-06 16:20:31 +0300

    consul stub auto configuration

--- fancy-discovery-consul-spring-boot-starter/src/main/java/com/jstik/site/discovery/stub/StubLoadBalancerConfig.java
+++ fancy-discovery-consul-spring-boot-starter/src/main/java/com/jstik/site/discovery/stub/StubLoadBalancerConfig.java
@@ -1,13 +1,15 @@
 package com.jstik.site.discovery.stub;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.consul.serviceregistry.ConsulRegistrationCustomizer;
+import org.springframework.cloud.consul.serviceregistry.ConsulServletRegistrationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.servlet.ServletContext;
 import java.util.List;
 
 @Configuration
@@ -25,6 +27,13 @@ public class StubLoadBalancerConfig {
     @ConfigurationProperties(prefix = "spring.cloud.consul.stub")
     public StubLoadBalancerProperties stubLoadBalancerProperties(){
         return new StubLoadBalancerProperties();
+    }
+
+
+    @Bean
+    @ConditionalOnClass({ServletContext.class})
+    public ConsulRegistrationCustomizer servletConsulCustomizer(ObjectProvider<ServletContext> servletContext) {
+        return new ConsulServletRegistrationCustomizer(servletContext);
     }
 
 }
